ab216ebcbc41c204abb4bb9ed79a0d44e562a692
Omar Barazanji <ozanji345@gmail.com>
2020-01-22 13:30:37 -0600

    Add files via upload

--- lab2.c
+++ lab2.c
@@ -1,22 +1,27 @@
 #include "STM32L1xx.h"
 
 static int counter; // Initialize count variable
+unsigned char direction; // direction
+
 
 void PinSetup () {
 	RCC->AHBENR |= 0x01; // Enable GPIOA clock (bit 0)
+	RCC->AHBENR |= 0x04;
 	GPIOA->MODER &= 0xFFFFFFC3; // General purpose input mode for PA1 and PA2
-	GPIOC->MODER &= 0xFFFFFF55; // output mode for PC0-3
+	GPIOC->MODER &= ~0xF;
+	GPIOC->MODER |= 0x00000055; // output mode for PC0-3
 }
 
 void delay () {
-	int stop = 16000000;
+	int stop = 1250000;
 	for (int i=0; i<stop; i++) {         // outer loop
 	}
 }
 
-void count (int direction) {
-	int sw1 = GPIOA->IDR &= 0x0002; // Read PA1 (on or off)
-	if (sw1) {
+void count (unsigned char direction) {
+	unsigned char sw1 = GPIOA->IDR &= 0x0002; // Read PA1 (on or off)
+	sw1 = sw1 >> 1;
+	if (sw1 == 1) {
 		if (direction == 0) { // forward
 			counter++;
 			if (counter == 10) {
@@ -28,21 +33,23 @@ void count (int direction) {
 				counter = 10;
 			}
 		}
-	GPIOC->ODR &= counter;
 	}
+  GPIOC->ODR &= ~0xF;
+	GPIOC->ODR |= counter;
 }
 
 int main(void) {
-	int direction; // direction
 	PinSetup();
 	direction = GPIOA->IDR &= 0x0004; // Read PA2 (direction 0-for 1-rev)
-	if (direction) {
+	direction = direction >> 2;
+	if (direction == 1) {
 		counter = 10;
 	} else {
 		counter = 0;
 	}
 	while (1) {  // main loop
 		direction = GPIOA->IDR &= 0x0004; // Read PA2 (direction 0-for 1-rev)
+		direction = direction >> 2;
 		count(direction);
 		delay();
 	}
